b9dde1559a838ef258587d68c99df9cdaa24a27e
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2023-09-29 10:47:21 +0900

    Revised Kadai1 DFS source code

--- kadai1/kadai1.c
+++ kadai1/kadai1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 #define BUFSIZE 100
+#define MAX_NEIGHBORS 10
 
 typedef struct node {
-    struct node * s; 
-    struct node * t;
     int id;
     int visited;
-} node_t, * node_tp;
+    int neighborCount;
+    struct node * neighbors[MAX_NEIGHBORS]; 
+} node_t;
 
-void printNode(node_tp node) {
-    printf("(%d, %d)\n", node->id, node->visited);
+void printNode(node_t * node) {
+    printf("(node %d, visited %d times)\n", node->id, node->visited);
     fflush(stdout);
 }
 
@@ -19,41 +20,58 @@ node_t nodes[BUFSIZE]; /* 大域変数（配列） */
 void initNodes(int n) {
     int i;
     for(i = 0; i<n; i++) {
-        nodes[i].id = i; nodes[i].visited = 0;
-        nodes[i].s = nodes[i].t = NULL;
+        nodes[i].id = i;
+        nodes[i].visited = 0;
+        nodes[i].neighborCount = 0;
     }
 }
 
-void link(node_tp node, node_tp s, node_tp t) {
-    node->s = s; node->t = t;
+/*  */
+void link(node_t * origin, node_t * destination) {
+    origin->neighbors[origin->neighborCount] = destination;
+    origin->neighborCount++;
 }
 
-void dfs(node_tp node) {
-    node->visited++;
+void dfs(node_t * node) {
     printNode(node);
-    if(node->visited == 1) {
-        node_tp s = node->s;
-        node_tp t = node->t;
-        if(s != NULL) dfs(s);
-        if(t != NULL) dfs(t);
+    node->visited++;
+    if (node->visited == 1) { /* node が初めて出た場合は… */
+        int n;
+        for (n = 0; n < node->neighborCount; n++) { /* …隣接するノードを再帰的に探索 */
+            node_t * next = node->neighbors[n];
+            dfs(next);
+        }
     }
+    /* 初めてじゃ無かった時、あるいは隣接するノード全部探索済みになった時に、呼出側に戻る */
 }
 
-void test1(void) { /* 2021 version */
+void test1(void) {
+    /* グラフの定義 */
     initNodes(4);
-    link(&nodes[0], &nodes[2], &nodes[1]);
-    link(&nodes[1], &nodes[2], &nodes[3]);
-    link(&nodes[2], NULL, &nodes[3]);
-    link(&nodes[3], &nodes[0], NULL);
-    dfs(&nodes[0]); /* ノード 0 から探索 */
+    link(&nodes[0], &nodes[2]); /* 0 -> 2 */
+    link(&nodes[0], &nodes[1]); /* 0 -> 2 */
+
+    link(&nodes[1], &nodes[2]); /* 1 -> 2 */
+    link(&nodes[1], &nodes[3]); /* 1 -> 3 */
+    
+    link(&nodes[2], &nodes[3]); /* 2 -> 3 */
+    
+    link(&nodes[3], &nodes[0]); /* 3 -> 0 */
+
+    /* 深さ優先探索 */
+    dfs(&nodes[0]); /* ノード 0 からスタート */
 }
 
 void test2(void) {
     int i;
-    initNodes(12);
-    for(i=0; i<10; i++) 
-        link(&nodes[i], &nodes[i+1], &nodes[i+2]);
-    dfs(&nodes[0]); /* ノード 0 から探索 */
+    initNodes(13); /* ノード 0 を使わない、ノード 1～12 を利用 */
+    for(i=3; i<13; i++) {
+        link(&nodes[i], &nodes[i-1]); /* i -> i-1 */
+        link(&nodes[i], &nodes[i-2]); /* i -> i-2 */
+    }
+    
+    /* 深さ優先探索 */
+    dfs(&nodes[12]); /* ノード 0 からスタート */
 }
 
 int main(void)  {
